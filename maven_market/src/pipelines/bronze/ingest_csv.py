619f070386b440f6a136533d1939c3a2a8fd7e44
Ansh2703 <anshtripathi220@gmail.com>
2026-04-22 06:59:33 +0000

    checking logger

--- maven_market/src/pipelines/bronze/ingest_csv.py
+++ maven_market/src/pipelines/bronze/ingest_csv.py
@@ -36,32 +36,32 @@ logger = PipelineLogger(layer="bronze")
 
 datasets = ["transactions", "regions", "stores", "return", "calendar"]
 
-# def create_bronze_table(dataset_name):
+def create_bronze_table(dataset_name):
 
-#     table_name = f"bronze_{dataset_name}"
-#     stage = table_name
+    table_name = f"bronze_{dataset_name}"
+    stage = table_name
 
-#     @dlt.table(
-#         name=table_name,
-#         comment=f"Raw ingestion of {dataset_name} data via Auto Loader."
-#     )
-#     def table_definition():
+    @dlt.table(
+        name=table_name,
+        comment=f"Raw ingestion of {dataset_name} data via Auto Loader."
+    )
+    def table_definition():
 
-#         logger.info("Starting ingestion", stage)
+        logger.info("Starting ingestion", stage)
 
-#         try:
-#             source_path = spark.conf.get(f"bundle.source_path_{dataset_name}")
+        try:
+            source_path = spark.conf.get(f"bundle.source_path_{dataset_name}")
 
-#             df = (
-#                 spark.readStream.format("cloudFiles")
-#                 .option("cloudFiles.format", "csv")
-#                 .option("header", "true")
-#                 .option("cloudFiles.inferColumnTypes", "true")
-#                 .load(source_path)
-#                 .withColumn("ingestion_time", current_timestamp())
-#                 .withColumn("source_file", col("_metadata.file_path"))
-#                 .withColumn("source_name", lit(dataset_name))
-#             )
+            df = (
+                spark.readStream.format("cloudFiles")
+                .option("cloudFiles.format", "csv")
+                .option("header", "true")
+                .option("cloudFiles.inferColumnTypes", "true")
+                .load(source_path)
+                .withColumn("ingestion_time", current_timestamp())
+                .withColumn("source_file", col("_metadata.file_path"))
+                .withColumn("source_name", lit(dataset_name))
+            )
 
             logger.info("Transformation defined", stage, status="SUCCESS")
             return df
